20a442669a2fc647a5fc96f85472137d75fc30ee
john lopez <johnguitarmusica@gmail.com>
2025-02-17 15:58:57 -0500

    commit 2.7

--- .vercel/output/_functions/entry.mjs
+++ .vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_6jfqFKRY.mjs';
-import { manifest } from './manifest_CZkCxMQ-.mjs';
+import { manifest } from './manifest_BxZbohk5.mjs';
 
 const serverIslandMap = new Map([
 	['BookScore', () => import('./chunks/BookScore_BRDxKXYb.mjs')],
@@ -23,7 +23,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "50769afd-777b-4ea7-bf58-2b8f8910b221",
+    "middlewareSecret": "6c82f49e-b905-45ba-be28-eb2c331a5eab",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
